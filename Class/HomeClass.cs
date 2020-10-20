37697a7b6403d2750dbe4fc3ee28906af87dd7c9
rahadianaldi <rahadianaldi@gmail.com>
2020-10-20 20:26:04 +0700

    First Commit

---
+++ Class/HomeClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M6_EAI_Scrapping_Output.Class
+{
+    public class HomeClass
+    {
+    }
+
+    public class Buku
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string price { get; set; }
+        public string sold { get; set; }
+        public string condition { get; set; }
+        public string assurance { get; set; }
+        public string weight { get; set; }
+        public string seller { get; set; }
+        public string description { get; set; }
+    }
+
+    public class ListBuku
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorDesc { get; set; }
+        public List<Buku> buku { get; set; }
+    }
+}
---
+++ Models/HomeContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using M6_EAI_Scrapping_Output.Class;
+using MySqlConnector;
+
+namespace M6_EAI_Scrapping_Output.Models
+{
+    public class HomeContext
+    {
+        string conn = "server=localhost;port=3306;user=root;password=;database=db_scrap";
+
+        public ListBuku GetListBuku()
+        {
+            using var connection = new MySqlConnection(conn);
+            ListBuku Res = new ListBuku();
+            List<Buku> buku = new List<Buku>();
+            try
+            {
+                connection.Open();
+                string strquery = "SELECT * FROM scrap_page_1";
+                var command = new MySqlCommand(strquery, connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Buku bk = new Buku();
+                    bk.id = Convert.ToString(reader["id"]);
+                    bk.title = Convert.ToString(reader["title"]);
+                    bk.price = Convert.ToString(reader["price"]);
+                    bk.sold = Convert.ToString(reader["sold"]);
+                    bk.condition = Convert.ToString(reader["condition"]);
+                    bk.assurance = Convert.ToString(reader["assurance"]);
+                    bk.weight = Convert.ToString(reader["weight"]);
+                    bk.seller = Convert.ToString(reader["seller"]);
+                    bk.description = Convert.ToString(reader["description"]);
+                    buku.Add(bk);
+                }
+                Res.ErrorCode = "0";
+                Res.ErrorDesc = "Success";
+                Res.buku = buku;
+            }
+            catch (Exception e)
+            {
+                Res.ErrorCode = "1";
+                Res.ErrorDesc = e.Message.ToString();
+                throw;
+            }
+
+            return Res;
+        }
+    }
+}
